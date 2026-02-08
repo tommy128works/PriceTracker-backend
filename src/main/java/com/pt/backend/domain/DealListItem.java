a4db376764faba3a672ec112e3e4979c59967766
Tommy <57922367+tommy128works@users.noreply.github.com>
2026-02-08 13:25:35 -0500

    feat: add DealListItem entity

---
+++ src/main/java/com/pt/backend/domain/DealListItem.java
@@ -0,0 +1,37 @@
+package com.pt.backend.domain;
+
+import com.pt.backend.domain.Deal.Deal;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "deal_list_items")
+@Getter
+@Setter
+@NoArgsConstructor
+public class DealListItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "deal_list_id", nullable = false)
+    private DealList dealList;
+
+    @NonNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "deal_id", nullable = false)
+    private Deal deal;
+
+    private String note;
+
+    @Builder
+    public DealListItem(DealList dealList, Deal deal, String note) {
+        this.dealList = dealList;
+        this.deal = deal;
+        this.note = note;
+    }
+
+}
