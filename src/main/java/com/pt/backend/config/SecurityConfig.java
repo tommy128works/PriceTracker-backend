37db09e3a38914859c438c37c342859c17f19b83
Tommy <57922367+tommy128works@users.noreply.github.com>
2026-02-22 11:36:52 -0500

    chore: add SecurityConfig for development

---
+++ src/main/java/com/pt/backend/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.pt.backend.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        // permit all requests for now
+                        // delete after auth is set up
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
